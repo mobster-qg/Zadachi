a7051805cb91436f17613fdc3dcc69ecec5c7f8b
Achelov efim <mobster-qg@@mail.ru>
2022-12-03 04:50:25 +0700

    з2

---
+++ z2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("a:");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("b:");
+
+int b = int.Parse(Console.ReadLine());
+
+    if( a > b )
+    {
+    Console.WriteLine("a больше b");
+    }
+    else
+    {
+        Console.WriteLine("a не больше b");
+    }
