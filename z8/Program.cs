fe759d532afa9f215b600378879f017590f96017
Achelov efim <mobster-qg@@mail.ru>
2022-12-03 05:00:57 +0700

    з8

---
+++ z8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+Console.WriteLine( "Введите число:" );
+
+int N = int.Parse(Console.ReadLine());
+int b = N;
+while( b % 2 == N)
+{
+    Console.Write($"{b}");
+    b++;
+}
